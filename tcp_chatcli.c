05da9141e69177f9c955098acb765eb4dffd1e1e
sanggoe <sanggoe0509@gmail.com>
2020-11-16 16:30:38 +0900

    11주차 수업, chat 프로그램

---
+++ tcp_chatcli.c
@@ -0,0 +1,105 @@
+//--------------------------------------------------------
+// 파일명 : tcp_chatcli.c
+// 기  능 : 서버에 접속한 후 키보드의 입력을 서버로 전달하고,
+// 	    서버로부터 오는 메시지를 화면에 출력한다
+// 컴파일 : cc -o tcp_chatcli tcp_chatcli.c
+// 사용법 : tcp_chatcli 127.0.0.1 4001 name
+//--------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <strings.h>
+#include <fcntl.h>
+#include <netdb.h>
+//#include <unistd.h>
+
+#define MAXLINE 1000
+#define NAME_LEN 20
+
+char *EXIT_STRING = "exit";
+int tcp_connect(int af, char *servip, unsigned short port); // 소켓 생성 및 서버 연결, 생성된 소켓 리턴
+void errquit(char *mesg) {	// 에러시 종료 함수
+	perror(mesg);
+	exit(1);
+}
+
+int main(int argc, char* argv[]) {
+	char bufall[MAXLINE + NAME_LEN], // 이름 + 메시지를 위한 버퍼
+	     *bufmsg;	// bufall에서 메시지 부분의 포인터
+	int maxfdp1,	// 최대 소켓 디스크립터
+	    s,		// 소켓
+	    namelen;	// 이름의 길이
+	fd_set read_fds;// 읽기를 감지할 fd_set 구조체
+
+	if(argc != 4) {
+		printf("사용법 : %s server_ip port name\n", argv[0]);
+		exit(0);
+	}
+
+	sprintf(bufall, "[%s] : ", argv[3]);	// bufall의 앞부분에 이름을 저장
+	namelen = strlen(bufall);
+	bufmsg = bufall + namelen;	// 메시지 시작부분 지정 (초기화)
+
+	s = tcp_connect(AF_INET, argv[1], atoi(argv[2]));
+	if(s == -1)
+		errquit("tcp_connect fail");
+
+	puts("서버에 접속되었습니다.");
+	maxfdp1 = s+1; // 고정 되어있다.
+	FD_ZERO(&read_fds);
+
+	while(1) {
+		FD_SET(0, &read_fds); // 키보드 fd 초기화
+		FD_SET(s, &read_fds); // 수신 fd 초기화
+		// 여기까지 하면 select 준비 완료
+		if(select(maxfdp1, &read_fds, NULL, NULL, NULL) < 0) {
+			errquit("select fail");
+		}
+		// 서버로부터 수신한 메시지이면 - 소켓 s의 read_fds 변화이면
+		if(FD_ISSET(s, &read_fds)) {
+			int nbyte;
+			if ((nbyte = recv(s, bufmsg, MAXLINE, 0)) < 0) {
+				bufmsg[nbyte] = 0;
+				puts(bufmsg); // 화면 출력
+			}
+		}
+
+		// 키보드로부터 입력이면 - 0번 fd의 read_fds 변화이면
+		if(FD_ISSET(0, &read_fds)) {
+			if(fgets(bufmsg, MAXLINE, stdin)) {
+				if(send(s, bufall, namelen + strlen(bufmsg), 0) < 0)
+					puts("Error : Write error on socket.");
+				if(strstr(bufmsg, EXIT_STRING) != NULL) {
+					puts("Good bye.");
+					close(s);
+					exit(0);
+				}
+			}
+		}
+
+	} // end of while
+	return 0;
+}
+
+// 소켓 생성 및 서버 연결, 생성된 소켓 리턴
+int tcp_connect(int af, char *servip, unsigned short port) { 
+	struct sockaddr_in servaddr;
+	int s;
+
+	// 소켓 생성
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		return -1;
+	}
+
+	// 채팅 서버의 소켓 주소 구조체 servaddr 초기화
+	bzero((char *)&servaddr, sizeof(servaddr));
+	servaddr.sin_family = af;
+	inet_pton(AF_INET, servip, &servaddr.sin_addr);
+	servaddr.sin_port = htons(port);
+
+	// connect 콜
+	if (connect(s, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+		return -1;
+	}
+	return s;
+}
--- tcp_chatserv.c
+++ tcp_chatserv.c
@@ -1,8 +1,8 @@
 //--------------------------------------------------------
 // 파일명 : tcp_chatserv.c
 // 기  능 : 채팅 참가자 관리, 채팅 메시지 수신 및 방송
-// 컴파일 : cc -o chat_server tcp_chatserv.c
-// 사용법 : chat_server 4001
+// 컴파일 : cc -o tcp_chatserv tcp_chatserv.c
+// 사용법 : tcp_chatserv 4001
 //--------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,25 +46,88 @@ int main(int argc, char* argv[]) {
 	// tcp_listen(host, port, backlog) 함수 호출
 	listen_sock = tcp_listen(INADDR_ANY, atoi(argv[1]), 5);
 	
+	while(1) {
+		FD_ZERO(&read_fds);
+		FD_SET(listen_sock, &read_fds);
+		for(i=0; i<num_chat; i++) {
+			FD_SET(clisock_list[i], &read_fds);
+	       	}
+		maxfdp1 = getmax() + 1; // maxfdp1 재계산
+		puts("wait for client");
+		// 여기까지 하면 client 기다릴 준비 완료
+		if(select(maxfdp1, &read_fds, NULL, NULL, NULL) < 0) { // block
+			errquit("select fail");
+		} // select return시 어떤 소켓에서 변화가 생겼다는 뜻!
+	
+		// listen_sock의 변화이면 새 클라이언트 추가
+		if(FD_ISSET(listen_sock, &read_fds)) {	
+			accp_sock = accept(listen_sock, (struct sockaddr*)&cliaddr, &addrlen);
+			if(accp_sock == -1) {
+				errquit("accept fail");
+			}
+			addClient(accp_sock, &cliaddr);
+			send(accp_sock, START_STRING, strlen(START_STRING), 0);
+			printf("%d번째 사용자 추가.\n", num_chat);
+		}
 
-	close(listen_sock);
+		// 클라이언트가 보낸 메시지를 모든 클라이언트에게 방송
+		for(i=0; i<num_chat; i++) {
+			// 특정 클라이언트의 소켓에 변화가 있으면
+			if(FD_ISSET(clisock_list[i], &read_fds)) {
+				nbyte = recv(clisock_list[i], buf, MAXLINE, 0);
+				if(nbyte <= 0) { // 메시지 문제 발생시
+					removeClient(i); // 클라이언트 중료
+					continue;
+				}
+				buf[nbyte] = 0;
+				// 종료문자 처리
+				if(strstr(buf, EXIT_STRING) != NULL) {
+					removeClient(i); // 클라이언트 종료
+					continue;
+				}
+				// 모든 채팅 참가자에게 메시지 방송
+				for(j=0; j<num_chat; j++) {
+					send(clisock_list[j], buf, nbyte, 0);
+				}
+				puts(buf);
+			}
+		}
+	} // end of while
 	return 0;
 }
 
 
 // 새로운 채팅 참가자 처리
 void addClient(int s, struct sockaddr_in *newcliaddr) {
-
+	char buf[20];
+	inet_ntop(AF_INET, &newcliaddr->sin_addr, buf, sizeof(buf));
+	printf("new client: %s\n", buf); //, %d\n", buf, ntohs(newcliaddr->sin_port));
+	// 채팅 클라이언트 목록에 추가
+	clisock_list[num_chat] = s;
+	num_chat++;
 }
 
 // 최대 소켓번호 찾기
 int getmax() {
-
+	// MInimum 소켓 번호는 가장 먼저 생성된 listen_sock
+	int max = listen_sock;
+	int i;
+	for (i=0; i<num_chat; i++) {
+		if(clisock_list[i] > max) {
+			max = clisock_list[i];
+		}
+	}
+	return max;
 }
 
 // 채팅 탈퇴 처리 함수
 void removeClient(int s) {
-
+	close(clisock_list[s]);
+	if(s != num_chat-1) {
+		clisock_list[s] = clisock_list[num_chat-1];
+	}
+	num_chat--;
+	printf("채팅 참가자 1명 탈퇴. 현재 참가자 수 = %d\n", num_chat);
 }
 
 // 소켓 생성 및 listen
@@ -74,8 +137,7 @@ int tcp_listen(int host, int port, int backlog){
 
 	sd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sd == -1) {
-		perror("socket fail");
-		exit(1);
+		errquit("socket fail");
 	}
 	// servaddr 구조체의 내용 셋팅
 	bzero((char *)&servaddr, sizeof(servaddr));
@@ -84,8 +146,7 @@ int tcp_listen(int host, int port, int backlog){
 	servaddr.sin_port = htons(port);
 
 	if (bind(sd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-		perror("bind fail");
-		exit(1);
+		errquit("bind fail");
 	}
 
 	// 클라이언트로부터 연결 요청을 기다림
